d77471fc44f534e2158e8bf34a9abe3b843b2285
Aleksandar <aleksandar.radisic@uns.ac.rs>
2022-04-12 19:41:49 +0200

    Initial spring project
    Title

---
+++ Spring/src/main/java/InzenjeringZnanja/model/Test.java
@@ -0,0 +1,29 @@
+package InzenjeringZnanja.model;
+
+public class Test {
+    private String ime;
+    private double visina;
+
+    public Test(String ime, double visina){
+        this.ime = ime;
+        this.visina = visina;
+    }
+
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public double getVisina() {
+        return visina;
+    }
+
+    public void setVisina(double visina) {
+        this.visina = visina;
+    }
+}
+
