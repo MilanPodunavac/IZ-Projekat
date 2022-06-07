2c2a548030912c10a95f3a88f0364062b867a0d1
Milan Podunavac <milan.podunavac@uns.ac.rs>
2022-06-08 01:24:40 +0200

    base flask and owlready setup

--- Python/main.py
+++ Python/main.py
@@ -1,19 +1,22 @@
+import string
+
 from flask import Flask, render_template, flash, redirect, url_for, request, session, logging
 from owlready2 import *
 
 
 app = Flask(__name__)
 onto_path.append("C:/Users/User/Desktop/Milan/IZ/IZ-Projekat")
-classes = get_ontology("https://raw.githubusercontent.com/MilanPodunavac/IZ-Projekat/main/ontology_classes_xml.owl")
+classes = get_ontology("https://raw.githubusercontent.com/MilanPodunavac/IZ-Projekat/feat/python-setup/ontology_classes_xml.owl")
 classes.load()
-instances = get_ontology("https://raw.githubusercontent.com/MilanPodunavac/IZ-Projekat/main/ontology_instances_xml.owl")
+instances = get_ontology("https://raw.githubusercontent.com/MilanPodunavac/IZ-Projekat/feat/python-setup/ontology_instances_xml.owl")
 instances.load()
 
 
 @app.route('/')
 def index():
-    processor = instances.Processor("AMD_Processor_A6-9500E")
-    return processor.has_a_name
+    processors = len(list(instances.individuals()))
+    #motherboard = instances.search_one(is_a = classes.Motherboard, has_a_name = "motherboard ASUS Matična ploča PRIME A320M-K")
+    return str(processors)
 
 
 if __name__ == '__main__':
