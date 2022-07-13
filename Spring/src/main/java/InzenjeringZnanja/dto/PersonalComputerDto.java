8479e1741109eaf49e7f0bf44aacf65646bd0565
Aleksandar Radišić <73358934+AleksandarRadisic@users.noreply.github.com>
2022-07-13 19:48:33 +0200

    Ne radi

---
+++ Spring/src/main/java/InzenjeringZnanja/dto/PersonalComputerDto.java
@@ -0,0 +1,14 @@
+package InzenjeringZnanja.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonalComputerDto {
+    private String cpuName;
+    private String gpuName;
+    private String mbName;
+    private String ramName;
+    private String diskName;
+}
